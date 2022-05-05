ca5b57b000b05afee2c41b555b7d4d0f345a88b1
Wonda <jk4837@yahoo.com.tw>
2022-05-06 01:50:27 +0800

    Edit TwoStateIconButton

--- include/CustomTypes/TwoStateIconButton.hpp
+++ include/CustomTypes/TwoStateIconButton.hpp
@@ -18,11 +18,14 @@ public:
     TwoStateIconButton(const std::string_view &name, UnityEngine::Transform *parent, const std::string_view &buttonTemplate,
                        const UnityEngine::Vector2 &anchoredPosition, const UnityEngine::Vector2 &sizeDelta,
                        const std::function<void(void)> &onClick1, UnityEngine::Sprite *icon1, const std::string &hint1,
-                       const std::function<void(void)> &onClick2, UnityEngine::Sprite *icon2, const std::string &hint2);
+                       const std::function<void(void)> &onClick2, UnityEngine::Sprite *icon2, const std::string &hint2,
+                       const bool autoChangeState = true);
 
     void SetIsFirstState(const bool isFirstState);
+    bool GetIsFirstState() { return isFirstState_; }
 
 private:
+    bool autoChangeState_ = true;
     bool isFirstState_ = true;
     std::string hint1_;
     std::string hint2_;
--- src/CustomTypes/TwoStateIconButton.cpp
+++ src/CustomTypes/TwoStateIconButton.cpp
@@ -11,8 +11,9 @@ namespace PlaylistEditor
 TwoStateIconButton::TwoStateIconButton(const std::string_view &name, UnityEngine::Transform *parent, const std::string_view &buttonTemplate,
                                        const UnityEngine::Vector2 &anchoredPosition, const UnityEngine::Vector2 &sizeDelta,
                                        const std::function<void(void)> &onClick1, UnityEngine::Sprite *icon1, const std::string &hint1,
-                                       const std::function<void(void)> &onClick2, UnityEngine::Sprite *icon2, const std::string &hint2)
-                                       : hint1_(hint1), hint2_(hint2), icon1_(icon1), icon2_(icon2),
+                                       const std::function<void(void)> &onClick2, UnityEngine::Sprite *icon2, const std::string &hint2,
+                                       const bool autoChangeState)
+                                       : hint1_(hint1), hint2_(hint2), icon1_(icon1), icon2_(icon2), autoChangeState_(autoChangeState),
                                          IconButton(name, parent, buttonTemplate, anchoredPosition, sizeDelta, nullptr, icon1, hint1)
 {
     std::function<void()> onClick = (std::function<void()>) [this, onClick1, onClick2] () {
@@ -20,7 +21,9 @@ TwoStateIconButton::TwoStateIconButton(const std::string_view &name, UnityEngine
             onClick1();
         else
             onClick2();
-        this->SetIsFirstState(!isFirstState_);
+
+        if (this->autoChangeState_)
+            this->SetIsFirstState(!isFirstState_);
     };
 
     this->btn_->get_onClick()->AddListener(PlaylistEditor::Utils::MakeDelegate<UnityEngine::Events::UnityAction*>(onClick));
