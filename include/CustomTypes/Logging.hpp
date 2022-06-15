e15a7420657c0d33ca1b4e55d1de850c11639c2e
Wonda <jk4837@yahoo.com.tw>
2022-06-15 21:15:12 +0800

    Add debug level log

--- include/CustomTypes/Logging.hpp
+++ include/CustomTypes/Logging.hpp
@@ -11,5 +11,6 @@ namespace PlaylistEditor
             static LoggerContextObject& getContextLogger(const char* fun, const char* file, int line);
     };
 }
+#define DEBUG(...) ::PlaylistEditor::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).debug(__VA_ARGS__)
 #define INFO(...) ::PlaylistEditor::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).info(__VA_ARGS__)
 #define ERROR(...) ::PlaylistEditor::Logging::getContextLogger(__PRETTY_FUNCTION__, __FILE__, __LINE__).error(__VA_ARGS__)
